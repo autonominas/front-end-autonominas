71d889ef2c7bc8bd01db65a9710ca5444ed79f13
unknown <isabella14brandao@gmail.com>
2022-10-06 10:27:20 -0300

    Criado componente models

---
+++ src/services/Service.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://autonominas.onrender.com'
+})
+
+    export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
+        const resposta = await api.post(url,dados)
+        setDado(resposta.data)
+    }
+
+    export const login = async(url: any,dados: any,setDado: any) => { 
+        const resposta = await api.post(url,dados)
+        setDado(resposta.data.token)
+    }
+
+
+
+
+
+
+
+
+
+
