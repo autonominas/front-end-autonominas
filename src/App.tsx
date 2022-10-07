a7722618dcb9e60fc7d5b64b205b1d35fc99908a
unknown <isabella14brandao@gmail.com>
2022-10-07 10:07:47 -0300

    adiciona componente para consumo de api

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import './App.css';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Login />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+        
         </Routes>
       </div>
       <Footer />
